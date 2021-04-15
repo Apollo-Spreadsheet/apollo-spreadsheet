018acf5dc1f04719d8595ea00fb2a7090c638954
VeraRibeiro <veribeiro.6@hotmail.com>
2021-04-15 11:47:14 +0100

    Stories and typedoc
    - Reordered example stories
    - Installed typedoc

--- stories/Examples.stories.tsx
+++ stories/Examples.stories.tsx
@@ -6,12 +6,11 @@ import { VPD } from './components/VPD'
 import { TableWithNestedRows } from './components/Table/TableWithNestedRows'
 
 const isDevelopmentOnly = process.env.NODE_ENV === 'development'
-storiesOf('Examples', module)
-  .add('Table', () => <Table />)
-  .add('Table with NestedRows', () => <TableWithNestedRows />)
-
 if (isDevelopmentOnly) {
   storiesOf('Examples', module)
     .add('Spreadsheet', () => <Spreadsheet />)
     .add('VPD', () => <VPD />)
 }
+storiesOf('Examples', module)
+  .add('Table', () => <Table />)
+  .add('Table with NestedRows', () => <TableWithNestedRows />)
