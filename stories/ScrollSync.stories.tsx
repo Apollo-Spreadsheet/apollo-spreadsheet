25e1902ba43557e41105480c380fc5ac4563fe7d
VeraRibeiro <veribeiro.6@hotmail.com>
2022-03-09 17:18:12 +0000

    Budget

--- stories/ScrollSync.stories.tsx
+++ stories/ScrollSync.stories.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { ScrollExample } from './components/ScrollSync/ScrollExample'
+import { BudgetTest } from './components/ScrollSync/BudgetTest'
 
-storiesOf('ScrollSync', module).add('scrollSync', () => <ScrollExample />)
+storiesOf('ScrollSync', module)
+  .add('scrollSync', () => <ScrollExample />)
+  .add('P&L', () => <BudgetTest />)
